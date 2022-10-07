d5063aa311a733380427fe4184b69fd6b6d68624
Syed Taha Khalid <tahatk20@gmail.com>
2022-10-07 21:16:18 +0500

    implement of coding task 1

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -101,7 +101,14 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        try {
+            //code...
+            $event = Event::with('workshops')->get();
+            return response()->json($event);
+        } catch (\Throwable $th) {
+            //throw $th;
+            throw new \Exception('implement in coding task 1');
+        }
     }
 
 
